6b76d7cfb71cd351d4ac5f3da2b0c8eddb782e98
cypherix93 <bikram.ghosh93@gmail.com>
2018-12-17 23:07:17 -0500

    Added game component for testing

---
+++ src/components/views/Game/Game.ts
@@ -0,0 +1,57 @@
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+import axios from 'axios';
+import * as signalr from '@aspnet/signalr';
+
+@Component
+export default class extends Vue {
+
+    Identity: any = null;
+
+    Connection: signalr.HubConnection = null;
+
+    Game: any = null;
+    GameLog: string[] = [];
+
+    IsReady: boolean = false;
+
+    async created() {
+        // Log In
+        await axios.post(AppConfig.ApiPath + '/auth/login', {
+            Email: 'npmgame',
+            Password: 'asdfghjkl'
+        });
+
+        // Show identity
+        const idResponse = await axios.get(AppConfig.ApiPath + '/auth/me');
+        this.Identity = idResponse.data;
+
+        // Establish connection
+        this.Connection = new signalr.HubConnectionBuilder()
+            .withUrl(AppConfig.ApiPath + '/games')
+            .build();
+
+        this.Connection.on('game:log', (message: string) => {
+            this.GameLog.push(message);
+        });
+
+        this.Connection.on('game:update', (game) => {
+            this.Game = game;
+        });
+
+        await this.Connection.start();
+
+        this.IsReady = true;
+    }
+
+    async JoinGame() {
+        const gameResponse = await axios.get(AppConfig.ApiPath + '/games/current');
+
+        await this.Connection.invoke('JoinGame', gameResponse.data.Id);
+    }
+
+    async StartGame() {
+        await this.Connection.invoke('StartGame');
+    }
+}
---
+++ src/components/views/Game/Game.vue
@@ -0,0 +1,22 @@
+<script lang="ts" src="./Game.ts"></script>
+
+<template>
+    <div class="GameComponent container" v-if="IsReady">
+        <h2>Welcome back, {{Identity.UserName}}</h2>
+
+        <hr>
+
+        <div class="row">
+            <div class="col">
+                <pre>
+                    {{Game}}
+                </pre>
+            </div>
+            <div class="col">
+                <pre>
+                    {{GameLog.join('\n')}}
+                </pre>
+            </div>
+        </div>
+    </div>
+</template>
